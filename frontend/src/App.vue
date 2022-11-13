d1a2b89c2e2dc32f6e6c27755daa3d2324f94d96
TCKACHIKSIS <71429999+TCKACHIKSIS@users.noreply.github.com>
2022-11-13 21:57:09 +1000

    create vue app

---
+++ frontend/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <n-config-provider :theme="this.theme"  style="height: 100%">
+    <n-layout>
+    <n-layout-header bordered>
+        <n-button @click="changeThemeToDark">
+          Dark
+        </n-button>
+        <n-button @click="this.theme = null">
+          Light
+        </n-button>
+    </n-layout-header>
+
+      <n-layout-content>
+        <router-view/>
+      </n-layout-content>
+
+    </n-layout>
+  </n-config-provider>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import {
+  darkTheme, NConfigProvider, NGradientText, NButton, NSpace, NCard, NLayout, NLayoutContent, NLayoutHeader
+} from "naive-ui";
+
+@Options({
+  components: {
+    NGradientText, NConfigProvider, NButton, NSpace, NCard, NLayout, NLayoutContent, NLayoutHeader
+  },
+})
+export default class App extends Vue {
+  theme = darkTheme
+  changeThemeToDark(){
+    this.theme = darkTheme;
+    return
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  height: 100vh;
+</style>
+<style scoped>
+.n-layout-header {
+  padding: 12px;
+}
+.n-layout-content{
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <n-space>
+      DUX
+    </n-space>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {NGradientText, NConfigProvider, NSpace, NCard, NButton} from 'naive-ui';
+import { darkTheme } from 'naive-ui';
+
+@Options({
+  components: {
+    HelloWorld, NGradientText, NConfigProvider, NSpace, NCard, NButton
+  },
+})
+export default class Home extends Vue {
+  theme = darkTheme
+}
+</script>
